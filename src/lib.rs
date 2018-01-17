81f5f15643dd8b61ca1e9e9633bddd7460f82ba3
cookie-s <cookie-s@users.noreply.github.com>
2018-01-18 05:33:48 +0900

    wip convert_to_machincode...

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,55 @@ type AsmOpList = Vec<asm::AsmOp>;
 type OpList = Vec<op::Op>;
 
 #[derive(Debug, Clone, Copy)]
-pub struct GReg(i32);
+pub struct GReg(u32);
 #[derive(Debug, Clone, Copy)]
-pub struct FReg(i32);
+pub struct FReg(u32);
 #[derive(Debug, Clone, Copy)]
-pub struct CReg(i32);
+pub struct CReg(u32);
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum SPReg {
     CTR,
     LK,
 }
-fn id_to_spreg(x: i32) -> SPReg {
+fn id_to_greg(x: u32) -> GReg {
+    match x {
+        0...31 => GReg(x),
+        _ => panic!(),
+    }
+}
+fn greg_to_id(x: GReg) -> u32 {
+    let GReg(x) = x;
+    x
+}
+fn id_to_freg(x: u32) -> FReg {
+    match x {
+        0...31 => FReg(x),
+        _ => panic!(),
+    }
+}
+fn freg_to_id(x: FReg) -> u32 {
+    let FReg(x) = x;
+    x
+}
+fn id_to_creg(x: u32) -> CReg {
+    match x {
+        0...7 => CReg(x),
+        _ => panic!(),
+    }
+}
+fn creg_to_id(x: CReg) -> u32 {
+    let CReg(x) = x;
+    x
+}
+fn id_to_spreg(x: u32) -> SPReg {
     match x {
         0b0100000000 => SPReg::LK,
         0b0100100000 => SPReg::CTR,
         _ => panic!("no such reg!"),
     }
 }
-fn spreg_to_id(r: SPReg) -> i32 {
+fn spreg_to_id(r: SPReg) -> u32 {
     match r {
         SPReg::LK => 0b0100000000,
         SPReg::CTR => 0b0100100000,
--- src/op.rs
+++ src/op.rs
@@ -1,4 +1,4 @@
-use super::{CReg, FReg, GReg, OpList, SPReg};
+use super::*;
 
 #[derive(Debug)]
 pub enum Condition {
@@ -52,4 +52,131 @@ pub enum Op {
     SC(),
 
     LONG(i32),
+}
+
+pub fn convert_to_machinecode(op: Op) -> u32 {
+    fn get_opcode(op: &Op) -> u32 {
+        match *op {
+            Op::CMPWI(_, _, _) => 11,
+            Op::ADDI(_, _, _) => 14,
+            Op::ADDIS(_, _, _) => 15,
+            Op::ORI(_, _, _) => 24,
+            Op::ANDI(_, _, _) => 28,
+            Op::ANDIS(_, _, _) => 29,
+            Op::BC(_, _, _) => 16,
+            Op::B(_, _) => 18,
+            Op::BSPR(_, _) => 19,
+            Op::LWZ(_, _, _) => 32,
+            Op::STW(_, _, _) => 36,
+            Op::LFS(_, _, _) => 48,
+            Op::STFS(_, _, _) => 52,
+            Op::SC() => 17,
+            Op::ADD(_, _, _)
+            | Op::SUBF(_, _, _)
+            | Op::NEG(_, _)
+            | Op::AND(_, _, _)
+            | Op::OR(_, _, _)
+            | Op::XOR(_, _, _)
+            | Op::SLW(_, _, _)
+            | Op::SRW(_, _, _)
+            | Op::CMP(_, _, _)
+            | Op::MFSPR(_, _)
+            | Op::MTSPR(_, _) => 31,
+            Op::FADD(_, _, _)
+            | Op::FSUB(_, _, _)
+            | Op::FMUL(_, _, _)
+            | Op::FDIV(_, _, _)
+            | Op::FCMP(_, _, _)
+            | Op::FNEG(_, _)
+            | Op::FMR(_, _) => 63,
+            Op::LONG(_) => 0, // TODO hoge
+        }
+    }
+    fn get_xocode(op: &Op) -> u32 {
+        match *op {
+            Op::ADD(_, _, _) => 266,
+            Op::SUBF(_, _, _) => 40,
+            Op::NEG(_, _) => 104,
+            Op::AND(_, _, _) => 28,
+            Op::OR(_, _, _) => 444,
+            Op::XOR(_, _, _) => 316,
+            Op::SLW(_, _, _) => 24,
+            Op::SRW(_, _, _) => 536,
+            Op::CMP(_, _, _) => 0,
+            Op::MFSPR(_, _) => 339,
+            Op::MTSPR(_, _) => 467,
+            Op::FADD(_, _, _) => 21,
+            Op::FSUB(_, _, _) => 20,
+            Op::FMUL(_, _, _) => 25,
+            Op::FDIV(_, _, _) => 18,
+            Op::FCMP(_, _, _) => 0,
+            Op::FNEG(_, _) => 40,
+            Op::FMR(_, _) => 72,
+            _ => 0,
+        }
+    }
+    fn to_bin(dat: &Vec<(u32, u32)>) -> u32 {
+        // FIXME kuso
+        {
+            let mut sum_sz = 0;
+            for &(_, sz) in dat {
+                sum_sz += sz;
+            }
+            assert_eq!(sum_sz, 32);
+        }
+        let mut dat = dat.clone();
+        if let Some(last) = dat.last_mut() {
+            let (bits, _) = *last;
+            *last = (bits, 0);
+        }
+
+        let mut res = 0;
+        for (bits, size) in dat {
+            res += bits;
+            res <<= size;
+        }
+        res
+    }
+
+    match op {
+        Op::CMPWI(_, _, _) => 11,
+        Op::ADDI(_, _, _) => 14,
+        Op::ADDIS(_, _, _) => 15,
+        Op::ORI(_, _, _) => 24,
+        Op::ANDI(_, _, _) => 28,
+        Op::ANDIS(_, _, _) => 29,
+        Op::BC(_, _, _) => 16,
+        Op::B(_, _) => 18,
+        Op::BSPR(_, _) => 19,
+        Op::LWZ(_, _, _) => 32,
+        Op::STW(_, _, _) => 36,
+        Op::LFS(_, _, _) => 48,
+        Op::STFS(_, _, _) => 52,
+        Op::SC() => 17,
+        Op::ADD(rt, ra, rb)
+        | Op::SUBF(rt, ra, rb)
+        | Op::AND(ra, rt, rb)
+        | Op::OR(ra, rt, rb)
+        | Op::XOR(ra, rt, rb)
+        | Op::SLW(ra, rt, rb)
+        | Op::SRW(ra, rt, rb) => to_bin(&vec![
+            (get_opcode(&op), 6),
+            (greg_to_id(rt), 5),
+            (greg_to_id(ra), 5),
+            (greg_to_id(rb), 5),
+            (0, 1),
+            (get_xocode(&op), 9),
+            (1, 1),
+        ]),
+        Op::NEG(rt, ra) => 0,
+        Op::MFSPR(rt, ra) | Op::MTSPR(rt, ra) => 0,
+        Op::CMP(cr, ra, rb) => 0,
+        Op::FCMP(cr, fra, frb) => 0,
+        Op::FNEG(frt, fra) | Op::FMR(frt, fra) => 0,
+        Op::FADD(frt, fra, frb)
+        | Op::FSUB(frt, fra, frb)
+        | Op::FMUL(frt, fra, frb)
+        | Op::FDIV(frt, fra, frb) => 0,
+        Op::LONG(_) => 0, // TODO hoge
+    }
 }
